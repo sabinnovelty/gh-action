f5580d8be4f2f46925fc13737dac6a26fc9f5037
sabin <sabin@noveltytechnology.com>
2023-11-17 18:26:26 +0545

    text update

--- src/components/HelpArea.jsx
+++ src/components/HelpArea.jsx
@@ -15,13 +15,13 @@ const HELP_ITEMS = [
   {
     id: 'h3',
     title: 'What is GitHub Actions?',
-    text: 'GitHub Actions is an automation service (or CI / CD service) that helps you automate repository-related workflows and processes.',
+    text: 'GitHub Actions is an automation service (or CI / CD service) that helps you automate repository-related workflows and processes....',
   },
 ];
 
 function HelpArea() {
   return (
-    <section data-testid="help-area" id="help-area">
+    <section data-testid='help-area' id='help-area'>
       {HELP_ITEMS.map((item) => (
         <HelpBox key={item.id} title={item.title} text={item.text} />
       ))}
